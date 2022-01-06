ce81365f7a9bb4bd24fc4389d38cdbbb0ab8e259
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2022-01-06 20:19:56 +0000

    fully working

--- Triangle.swift
+++ Triangle.swift
@@ -6,20 +6,81 @@
 * @since   2021-12-15
 */
 
+import Foundation
+
 // The triangle class
 class Triangle {
-    private(set) var speed: Int
-    let maxSpeed: Int
-    private(set) var quantityWheels: Int
-    var colour: String
+    // Variables/Constants
+    private(set) var lengthA: Double
+    private(set) var lengthB: Double
+    private(set) var lengthC: Double
+    private(set) var area: Double
+    private(set) var angleA: Double
+    private(set) var angleB: Double
+    private(set) var angleC: Double
+    private(set) var name: String
+    private(set) var perimeter: Double
+    private(set) var semiperimeter: Double
+    private(set) var degrees: Double
 
     // The constructor
-    init (startColour: String, startMaxSpeed: Int, startLicensePlate: String,
-          initialQuantityWheels: Int) {
-        licensePlate = startLicensePlate
-        colour = startColour
-        maxSpeed = startMaxSpeed
-        quantityWheels = initialQuantityWheels
-        speed = 0
+    init (lengthInputA: Double, lengthInputB: Double, lengthInputC: Double) {
+        // Variables/Constants
+        let ninetyDegrees: Double = 90
+        let oneEighty: Double = 180
+
+        lengthA = lengthInputA
+        lengthB = lengthInputB
+        lengthC = lengthInputC
+
+        degrees = oneEighty / Double.pi
+
+        // The "degrees" changes the result from a radian to a degree
+        angleA = degrees * (acos((pow(lengthB, 2)
+            + pow(lengthC, 2) - pow(lengthA, 2))
+            / (2 * lengthB * lengthC)))
+
+        angleB = degrees * (acos((pow(lengthA, 2)
+            + pow(lengthC, 2) - pow(lengthB, 2))
+            / (2 * lengthA * lengthC)))
+
+        angleC = degrees * (acos((pow(lengthA, 2)
+            + pow(lengthB, 2) - pow(lengthC, 2))
+            / (2 * lengthA * lengthB)))
+
+        /*
+        * Name
+        */
+
+        // If all of the angles are acute
+        if angleA < ninetyDegrees && angleB < ninetyDegrees
+            && angleC < ninetyDegrees {
+            name = "Acute "
+        // If any of the angles are obtuse
+        } else if angleA > ninetyDegrees || angleB > ninetyDegrees
+            || angleC > ninetyDegrees {
+            name = "Obtuse "
+        // If there is a right angle
+        } else {
+            name = "Right "
+        }
+
+        // If all the angles are equal
+        if angleA == angleB && angleB == angleC {
+            name += "Equilateral "
+        // If only two of the angles are equal
+        } else if angleA == angleB || angleB == angleC
+            || angleA == angleC {
+            name += "Isosceles"
+        // If none of the angles are equal
+        } else {
+            name += "Scalene"
+        }
+
+        perimeter = lengthA + lengthB + lengthC
+        semiperimeter = perimeter / 2
+
+        area = (semiperimeter * (semiperimeter - lengthA) * (semiperimeter
+            - lengthB) * (semiperimeter - lengthC)).squareRoot()
     }
 }
--- main.swift
+++ main.swift
@@ -9,6 +9,87 @@
 //  swiftc -o combined *.swift; ./combined
 //  This program uses a custom class.
 
+// Making an error case.
+enum MyError: Error {
+    case invalidInteger(String)
+}
+
+func isValid(lengthA: Double, lengthB: Double, lengthC: Double) -> Bool {
+    var returnBool: Bool = false
+
+    // If any two sides are less than the other
+    if lengthA + lengthB < lengthC || lengthA + lengthC < lengthB
+        || lengthB + lengthC < lengthA {
+
+        // The triangle is invalid
+        print("\nInvalid inputs\n")
+
+    // If the triangle either isn't set or is set to an impossible length
+    } else if lengthA <= 0 || lengthB <= 0 || lengthC <= 0 {
+        // The triangle is invalid
+        print("\nPlease use real lengths\n\n")
+        returnBool = false
+    } else {
+
+        // The triangle is valid
+        returnBool = true
+    }
+
+    return returnBool
+}
+
+var triangleValid: Bool = false
+var sideA: Double = 0
+var sideB: Double = 0
+var sideC: Double = 0
+
+print("Please input the three sides")
+
+while !triangleValid {
+    do {
+
+        // Inputs
+        print("\nSide A: ")
+
+        // Unwraps the input
+        if let sideInputA: Double = Double(readLine()!) {
+            sideA = sideInputA
+        } else {
+            throw MyError.invalidInteger("Error")
+        }
+
+        print("\nSide B: ")
+
+        // Unwraps the input
+        if let sideInputB: Double = Double(readLine()!) {
+            sideB = sideInputB
+        } else {
+            throw MyError.invalidInteger("Error")
+        }
+
+        print("\nSide C: ")
+
+        // Unwraps the input
+        if let sideInputC: Double = Double(readLine()!) {
+            sideC = sideInputC
+        } else {
+            throw MyError.invalidInteger("Error")
+        }
+
+    } catch {
+        print("Please input again")
+    }
+
+    triangleValid = isValid(lengthA: sideA, lengthB: sideB, lengthC: sideC)
+}
+
 // Defining variables
-let fullTriangle: Triangle = Triangle(startColour: "Blue", startMaxSpeed: 210,
-                         startLicensePlate: "HFEIK", initialQuantityWheels: 4)
+let fullTriangle: Triangle = Triangle(lengthInputA: sideA,
+    lengthInputB: sideB, lengthInputC: sideC)
+
+print("The area is: ", fullTriangle.area)
+print("Angle A is: ", fullTriangle.angleA)
+print("Angle B is: ", fullTriangle.angleB)
+print("Angle C is: ", fullTriangle.angleC)
+print("The name is: ", fullTriangle.name)
+print("The perimeter is: ", fullTriangle.perimeter)
