89387501ac19bf747bd1772504b0a15edf6dfa68
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-01-06 14:32:54 -0500

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,14 @@
+// 
+//  Created by Jonathan Pasco-Arnone
+//  Created on 2021-12-16
+//  Version 1.0
+//  Copyright (c) 2021 Jonathan Pasco-Arnone. All rights reserved.
+//
+//  In order to compile and run these files,
+//  I had to use the following command:
+//  swiftc -o combined *.swift; ./combined
+//  This program uses a custom class.
+
+// Defining variables
+let fullTriangle: Triangle = Triangle(startColour: "Blue", startMaxSpeed: 210,
+                         startLicensePlate: "HFEIK", initialQuantityWheels: 4)
